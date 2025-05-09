2124cf6e4cb5bfc2b57c86668a6bf20f1d010537
Honas <ge65gom@mytum.de>
2025-05-09 13:12:48 +0200

    final commit

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,10 +2,10 @@
     import { onMount } from "svelte";
     import "../app.css";
     import Icon from "@iconify/svelte";
-    import { env } from '$env/dynamic/public';
+    import { env } from "$env/dynamic/public";
 
-    import FoodCard from './FoodCard.svelte';
-    import type { Meal } from '$lib/types';
+    import FoodCard from "./FoodCard.svelte";
+    import type { Meal } from "$lib/types";
 
     // Use environment variable for API URL with fallback to localhost
     let baseUrl = env.PUBLIC_API_URL || "http://localhost:8080";
@@ -15,7 +15,17 @@
 
     // Fetch data once on component mount
     onMount(async () => {
-       // TODO Fetch meals from the API running on the baseUrl
+        async function fetchMeals() {
+            const res = await fetch(`${baseUrl}/mensa-garching/today`);
+            if (res.ok) {
+                meals = await res.json();
+            }
+        }
+
+        // Fetch data once on component mount
+        onMount(async () => {
+            await fetchMeals();
+        });
     });
 </script>
 
@@ -30,7 +40,11 @@
             <p>Loading menu items...</p>
         </div>
     {:else}
-       <!-- TODO add food-grid here -->
+        <div class="food-grid">
+            {#each meals as meal}
+                <FoodCard {meal}/>
+            {/each}
+        </div>
     {/if}
 
     {#if meals.length === 0 && meals.length > 0}
